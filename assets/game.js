1ba1c634cdaef84047ff8007fbb839be59a58ba9
jengle-dev <j.engle.dev@gmail.com>
2022-12-18 12:24:00 -0600

    solution from Richard tweaked for myself. tried explicit === and then ==

--- assets/game.js
+++ assets/game.js
@@ -1,29 +1,50 @@
-// Must use the `alert()`, `confirm()`, and `prompt()` methods to collect user input and display information to the user.
-// window.alert("Want to play a game?");
-// window.prompt("Select Rock, Paper, or Scissors");
-// window.confirm("Play again?");
+//keep score
+var wins = 0;
+var ties = 0;
+var losses = 0;
 
-// window.alert("You have now entered a game of Rock Paper Scissors against the computer")
-// // Choice
-// window.prompt("Please input r for rock, p for paper and s for scissors. Click OK to enter.")
+var choice = ["r", "p", "s"]
 
-let choice = prompt("Please input r for rock, p for paper and s for scissors. Click OK to enter.", "")
+var playGame = function () {
+    var userChoice = window.prompt("Please type r for rock, p for paper, s for scissors and click OK.");
 
-
-while (choice.toLowerCase != null) {
-    if (choice.toLowerCase === "r") {
-        window.alert("You have selected rock.")
+    if (!choice) {
+        return;
     }
-    else if (choice.toLowerCase === "p") {
-        window.alert("You have selected paper.")
-    }
-    else if (choice.toLowerCase === "s") {
-        window.alert("You have selected scissors.")
-    }
+
+    choice = choice.toLowerCase(); {
+        var randomChoice = Math.floor(Math.random() * choice.length);
+        var compChoice = choice[randomChoice];
+
+        window.alert("You chose" + choice + "." + " The computer chose" +compChoice +".")
+
+        if (choice === compChoice) {
+            ties++;
+            window.alert("It's a tie! Try again.")
+        }
+
+        else if (
+            (choice == "r" && compChoice === "s") ||
+            (choice == "p" && compChoice === "r") ||
+            (choice == "s" && compChoice === "p") 
+            ) {
+                wins++
+                window.alert("You are the winner!");
+        }
+
+        else {
+            losses++;
+            window.alert("I'm sorry. You did not win.")
+        }
+        window.alert("Scoreboard: \nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
+
+        var playAgain = window.confirm("Would you like to play again?")
+        if (playAgain){
+            playGame();
+            }
+        else {
+            window.close();
+            }
+        }
 }
-
-// get rock, paper, scissors choice
-
-// computer compares input value against computer output
-
-// computer displays results
+playGame();
